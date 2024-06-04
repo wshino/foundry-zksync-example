5ee90ccf2e089912b8a3acfe0b8140dec4aaafb6
wshino <wshinohara@gmail.com>
2024-06-05 01:42:21 +0900

    first commit.

--- contracts/src/Counter.sol
+++ contracts/src/Counter.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
 contract Counter {
     uint256 public number;
 
